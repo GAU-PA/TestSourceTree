c8eabdab897821eca2278ed00795e6b8da3b74eb
PC_Fenris <msblade117@hotmail.fr>
2017-10-31 15:26:44 +0100

    Test de SourceTree update prixTTC

--- HelloWorld.pas
+++ HelloWorld.pas
@@ -2,8 +2,18 @@ PROGRAM hello_world;
 
 USES crt;
 
+//but: obtenir un prix TTC à partir d'un prix HT
+//principe: Multiplier le prix HT avec la taxe pour obtenir le prix TTC
+
+VAR
+	PrixHT, PrixTTC:real;
+
 BEGIN
 	clrscr;
-	writeln('hello world !');
-	readln;
+
+	writeln('Entrer le prix HT');
+	readln(PrixHT);
+	PrixTTC:=PrixHT*1.2;
+	writeln('Le prix avec tax est: ', PrixTTC);
+	Readkey;
 END.
